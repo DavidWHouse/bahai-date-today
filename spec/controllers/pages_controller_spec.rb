a074a5ae8c0dc3a993a4bc276cd3583944d5b009
Lessan Vaezi <lessan@gmail.com>
2015-07-07 16:50:16 +0800

    Add static pages: about, bahai-calendar

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe HighVoltage::PagesController, '#show' do
+  %w(about bahai-calendar).each do |page|
+    context 'on GET to /pages/#{page}' do
+      before do
+        get :show, id: page
+      end
+
+      it { expect(response).to have_http_status(:success) }
+      it { expect(response).to render_template(page) }
+    end
+  end
+end
