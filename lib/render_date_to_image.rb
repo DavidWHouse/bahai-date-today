1968241e16c5c7edbb6c53589d8a21ec8ed79fd4
Lessan Vaezi <lessan@gmail.com>
2015-07-08 00:16:30 +0800

    Image renderer now returns a path

--- lib/render_date_to_image.rb
+++ lib/render_date_to_image.rb
@@ -3,12 +3,13 @@ require 'imgkit'
 require 'bahai_date'
 
 class RenderDateToImage
-  attr_accessor:date, :template, :html
+  attr_accessor:date, :target_path, :template, :html
 
   TEMPLATE_PATH = 'app/assets/htmls/date.html.erb'
 
-  def perform(year, month, day)
+  def perform(year, month, day, rails_root)
     set_date(year, month, day)
+    set_path(rails_root)
     load_template
     render_template_to_html
     render_html_to_image
@@ -16,6 +17,11 @@ class RenderDateToImage
 
   def set_date(year, month, day)
     @date = BahaiDate::BahaiDate.new(year: year, month: month, day: day)
+  end
+
+  def set_path(rails_root)
+    filename = "#{@date.to_s}.jpg"
+    @target_path = Pathname.new(rails_root).join('tmp', filename).to_s
   end
 
   def load_template
@@ -29,6 +35,6 @@ class RenderDateToImage
   def render_html_to_image
     options = { 'height' => 506, 'width' => 968, 'quality' => 75 }
     kit = IMGKit.new(@html, options)
-    kit.to_img
+    kit.to_file(@target_path)
   end
 end
--- spec/render_date_to_image_spec.rb
+++ spec/render_date_to_image_spec.rb
@@ -8,6 +8,15 @@ describe RenderDateToImage do
     expect(renderer.date.to_s).to eq('1.1.1')
   end
 
+  it 'sets the path' do
+    date = double
+    renderer.date = date
+    allow(date).to receive(:to_s).and_return('date')
+    renderer.set_path('root')
+    expect(renderer.target_path).to eq('root/tmp/date.jpg')
+  end
+
+
   it 'loads the template' do
     renderer.load_template
     expect(renderer.template).to include('div')
@@ -21,17 +30,25 @@ describe RenderDateToImage do
   end
 
   it 'renders the html to a jpg' do
-    renderer.html = '<html><body>Test</body></html>'
-    result = renderer.render_html_to_image
-    expect(result.size).to be > 1000
+    begin
+      temp_file = Tempfile.new(%w(rspec .jpg))
+      renderer.target_path = temp_file.path
+      renderer.html = '<html><body>Test</body></html>'
+      result = renderer.render_html_to_image
+      expect(File.read(result).size).to be > 1000
+    ensure
+      temp_file.close
+      temp_file.unlink
+    end
   end
 
   it 'performs all steps and returns the image' do
     expect(renderer).to receive(:set_date).with(1, 2, 3)
+    expect(renderer).to receive(:set_path).with('root')
     expect(renderer).to receive(:load_template)
     expect(renderer).to receive(:render_template_to_html)
-    expect(renderer).to receive(:render_html_to_image).and_return('image')
-    result = renderer.perform(1, 2, 3)
-    expect(result).to eq('image')
+    expect(renderer).to receive(:render_html_to_image).and_return('image_path')
+    result = renderer.perform(1, 2, 3, 'root')
+    expect(result).to eq('image_path')
   end
 end
