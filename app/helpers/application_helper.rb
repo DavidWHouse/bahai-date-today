6c8f41e6922a9e07b2af9420f95081843b365f0f
Lessan Vaezi <lessan@gmail.com>
2015-07-25 13:45:18 +0800

    Implement back and forward links for changing the date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     return "Baha'i Date Today" unless title.present?
     "#{title} — Baha'i Date Today"
   end
+
+  def date_as_params(date)
+    { year: date.year.bahai_era,
+      month: date.month.number,
+      day: date.day.number }
+  end
 end
