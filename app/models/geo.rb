07c5d283871e53407ca81e68cfc3d91931d67841
Lessan Vaezi <lessan@gmail.com>
2015-07-26 21:50:48 -0400

    Refine geo IP lookup

--- app/models/geo.rb
+++ app/models/geo.rb
@@ -3,17 +3,14 @@ require 'bahai_date'
 require 'solareventcalculator'
 
 class Geo
-  # source: http://www.timeanddate.com/astronomy/about-sun-calculator.html
-  # Technically, sunrise and sunset are calculated based on the true geocentric position of the Sun at 90°50' from the zenith position (directly above the observer).
-  AZIMUTH = 90.83
-
   # This product includes GeoLite2 data created by MaxMind, available from
   # <a href="http://www.maxmind.com">http://www.maxmind.com</a>.
   # http://dev.maxmind.com/geoip/geoip2/geolite2/
   # http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
   MAXMIND_DATA_FILE = '/usr/share/GeoIP/GeoLite2-City.mmdb'
 
-  attr_reader :ip, :latitude, :longitude, :city, :country, :time_zone, :current_time, :sunset_time_today, :current_date
+  attr_reader :ip, :latitude, :longitude, :city, :country, :time_zone,
+    :current_time, :sunset_time_today, :current_date, :current_gregorian_date_in_bahai_calendar
 
   def initialize(ip)
     @ip = ip
@@ -22,7 +19,7 @@ class Geo
   end
 
   def bahai_date
-    BahaiDate::BahaiDate.new(date: @current_date)
+    BahaiDate::BahaiDate.new(date: @current_gregorian_date_in_bahai_calendar || Date.today)
   end
 
   private
@@ -38,27 +35,28 @@ class Geo
     end
 
     def calculate_times_for_current_geo
-      @current_time = get_current_time + 5.hours
-      @sunset_time_today = get_sunset_time_today
+      @current_time = get_current_time_in_time_zone
       @current_date = get_current_date
+      @sunset_time_today = get_sunset_time_today
+      @current_gregorian_date_in_bahai_calendar = get_current_gregorian_date_in_bahai_calendar
     end
 
-    def get_current_time
-      return Time.now unless @time_zone
+    def get_current_time_in_time_zone
       Time.now.in_time_zone @time_zone
     end
 
-    def get_sunset_time_today
-      return @current_time unless @time_zone && @latitude && @longitude
-      calculator = SolarEventCalculator.new(@current_time.to_date, @latitude, @longitude)
-      calculator.compute_utc_solar_event(AZIMUTH, false).in_time_zone(@time_zone)
+    def get_current_date
+      @current_time.to_date
     end
 
-    def get_current_date
-      current_date = @current_time.to_date
-      return current_date unless @latitude && @longitude
-      return current_date unless sunset_passed?
-      current_date + 1.day
+    def get_sunset_time_today
+      calculator = SolarEventCalculator.new(@current_date, @latitude, @longitude)
+      calculator.compute_official_sunset(@time_zone)
+    end
+
+    def get_current_gregorian_date_in_bahai_calendar
+      return @current_date unless sunset_passed?
+      @current_date + 1.day
     end
 
     def sunset_passed?
